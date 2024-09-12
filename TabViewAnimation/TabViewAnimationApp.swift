6e956b8bfab295baafca62c68afa6d5549a47107
Musk <inhoyoo1029@gmail.com>
2024-09-12 18:30:11 +0900

    Initial Commit

---
+++ TabViewAnimation/TabViewAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewAnimationApp.swift
+//  TabViewAnimation
+//
+//  Created by Musk on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TabViewAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
